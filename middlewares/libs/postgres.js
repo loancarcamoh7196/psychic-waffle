7cca7c3a2efa85c14fdc0938d44a1ce5116c0395
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-11-30 10:24:45 -0300

    [imp-postgreSQL] Agregar controlador/services de restos de schemas

---
+++ middlewares/libs/postgres.js
@@ -0,0 +1,19 @@
+/**
+ * Librería de Conexion con PostgreSQL
+ * API con  [Docker] - [PostgreSQL]
+ */
+const { Client } = require('pg');
+
+async function getConnection(){
+    const client  = new  Client ({
+        host: 'localhost',
+        port: 5432,
+        user: 'lorena',
+        password: 'admin123',
+        database: 'my-store'
+    });
+    
+    await client.connect();
+}
+
+module.exports = getConnection;
