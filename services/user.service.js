e6bd020c339eb20bd192df1f3d3e22363d076c4d
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-11-30 10:25:37 -0300

    [imp-postgreSQL] Implementacion de postegreSQL en proyecto

---
+++ services/user.service.js
@@ -0,0 +1,34 @@
+const boom = require('@hapi/boom');
+
+const getConnection = require('../libs/postgres'); // Conexion con BD
+
+class UserService {
+    constructor() {}
+
+    async create(data) {
+        return data;
+    }
+
+    async find() {
+        const client = await getConnection();
+        const rta = await client.query('SELECT * FROM task');
+        return rta.rows;
+    }
+
+    async findOne(id) {
+        return { id };
+    }
+
+    async update(id, changes) {
+        return {
+            id,
+            changes,
+        };
+    }
+
+    async delete(id) {
+        return { id };
+    }
+}
+
+module.exports = UserService;
