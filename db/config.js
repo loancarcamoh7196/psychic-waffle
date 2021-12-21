8476135c6c0d5d543cd8aa4358daa1dc0fb1f166
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2021-12-21 13:06:29 -0300

    [imp-postgreSQL] Confiugracion de sequelize para conexion a BD produccion

--- db/config.js
+++ db/config.js
@@ -26,6 +26,7 @@ module.exports = {
         dialect: 'postgres',
         dialectOptions: {
             ssl: {
+                require: true,
                 rejectUnauthorized: false
             }
         }
