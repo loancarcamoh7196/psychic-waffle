ab963a501fd9261492cc9ffee3afc87163ac8710
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2022-01-06 12:46:54 -0300

    [passport-jwt] Cambios en seeders

--- db/seeders/20211210124408-add-user-data.js
+++ db/seeders/20211210124408-add-user-data.js
@@ -1,31 +1,27 @@
 'use strict';
+const bcrypt = require('bcrypt');
 const { Sequelize } = require('sequelize');
 const  {USER_TABLE } = require('../models/user.model');
 
 module.exports = {
   up: async (queryInterface) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-   await  queryInterface.bulkInsert(USER_TABLE, [{
-        email: 'root@yardsale.cl',
-        password: 'mio_mio#71',
-        created_at: Sequelize.fn('NOW')
-    },{
-        email: 'lo@yardsale.cl',
-        password: 'mio_mio#71',
-        created_at: Sequelize.fn('NOW')
-    },{
-        email: 'lo@love.cl',
-        password: 'mio_mio#71',
-        created_at: Sequelize.fn('NOW')
-    },
+    const hash = await bcrypt.hash('mio_mio#71', 10);
+    await  queryInterface.bulkInsert(USER_TABLE, [
+        {
+            email: 'root@yardsale.cl',
+            role: 'admin',
+            password: hash,
+            created_at: Sequelize.fn('NOW')
+        },{
+            email: 'lo@yardsale.cl',
+            role: 'seller',
+            password: hash,
+            created_at: Sequelize.fn('NOW')
+        },{
+            email: 'lo@love.cl',
+            password: hash,
+            created_at: Sequelize.fn('NOW')
+        },
     ],{});
   },
 
