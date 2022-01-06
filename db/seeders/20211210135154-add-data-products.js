1b532514baf96599a11219a24db3c98464c14873
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2022-01-06 13:54:09 -0300

    [passpportjs-jwt] Config seed

--- db/seeders/20211210135154-add-data-products.js
+++ db/seeders/20211210135154-add-data-products.js
@@ -9,21 +9,21 @@ module.exports = {
         [
             {
                 name: 'Product 1',
-                price: 0,
+                price: 10,
                 description: 'Pretty product',
                 image: 'http://placeimg.com/640/480',
                 created_at: Sequelize.fn('NOW'),
                 category_id: 1
             }, {
                 name: 'Product 3',
-                price: 0,
+                price: 5,
                 description: 'Pretty product',
                 image: 'http://placeimg.com/640/480',
                 created_at: Sequelize.fn('NOW'),
                 category_id: 1
             }, {
                 name: 'Product 2',
-                price: 0,
+                price: 71,
                 description: 'Pretty product',
                 image: 'http://placeimg.com/640/480',
                 created_at: Sequelize.fn('NOW'),
